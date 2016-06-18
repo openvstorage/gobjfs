e5e05b807e4a1488fa0986ca36bcd0f89d13f803
zensan <sanjos100@gmail.com>
2016-06-18 19:21:36 +0530

    In FileDistributor::removeDirectories, check that directories exist before calling nftw() to avoid spurious errors.
    If removing the top-level directory fails, do not flag error
    if its a mountpoint
    
    In FileDistributor::initDirectories(), create the top-level
    directories if they dont exist

--- src/FileDistributor.cpp
+++ src/FileDistributor.cpp
@@ -32,6 +32,28 @@ FileDistributor::initDirectories(const Config& config, bool createFlag)
 
   for (auto& mnt : config_.mountPoints_)
   {
+    // check if dir exists 
+    struct stat statBuf;
+    ret = stat(mnt.c_str(), &statBuf);
+
+    if (ret == -1) {
+      if (errno == ENOENT) {
+        // lets try to create the dir
+        ret = mkdir(mnt.c_str(), S_IRWXU | S_IRWXO | S_IRWXG);
+        if (ret == -1) {
+          LOG(ERROR) << "Failed to create directory=" << mnt
+            << " errno=" << errno;
+        }
+      } else {
+        LOG(ERROR) << "Failed to stat() directory=" << mnt
+          << " errno=" << errno;
+      }
+    } 
+
+    if (ret == -1) {
+      break;
+    }
+
     for (size_t idx = 0; idx < slotsPerMountPoint; idx ++)
     {
       auto dir = mnt + "/dir" + std::to_string(idx) + "/";
@@ -79,16 +101,54 @@ FileDeleterFunc(const char* fpath,
   if (typeflag == FTW_F) 
   {
     ret = ::unlink(fpath);
+    if (ret == 0) {
+      return FTW_CONTINUE; 
+    } 
   }
+
   else if (typeflag == FTW_DP)
   {
     ret = ::rmdir(fpath);
     VLOG(2) << "deleting dir=" << fpath << std::endl;
+
+    if (ret == 0) {
+      return FTW_CONTINUE;
+    } else if (ftwbuf->level == 0) {
+      // level=0 implies top-level dir
+      // if we fail to delete the top-level dir
+      // it could be because its a mount-point
+      // lets check and continue if thats the case
+      struct stat mntBuf;
+      struct stat parentDirBuf;
+
+      std::string mnt(fpath);
+
+      std::string parentDir = mnt + "/..";
+
+      // TODO : move this code to os_utils
+      ret = stat(mnt.c_str(), &mntBuf);
+
+      if (ret == -1) {
+        LOG(ERROR) << "Failed to stat dir=" << mnt
+          << " errno=" << errno;
+        return FTW_STOP;
+      }
+
+      ret = stat(parentDir.c_str(), &parentDirBuf);
+
+      if (ret == -1) {
+        LOG(ERROR) << "Failed to stat dir=" << parentDir
+          << " errno=" << errno;
+        return FTW_STOP;
+      }
+
+      bool isMountPoint = (mntBuf.st_dev != parentDirBuf.st_dev);
+
+      if (isMountPoint) 
+        return FTW_CONTINUE;
+    }
   }
-  if (ret == 0)
-  {
-    return FTW_CONTINUE; 
-  }
+
   LOG(ERROR) << "stopping.  Failed to delete " << fpath << std::endl;
   return FTW_STOP;
 }
@@ -100,25 +160,29 @@ FileDistributor::removeDirectories(
   const int numOpenFD = MaxOpenFd;
   int ret = 0;
 
-  for (auto dir : mountPoints)
+  for (auto mnt : mountPoints)
   {
+    // check if dir exists and can be written to
+    ret = access(mnt.c_str(), W_OK | X_OK | R_OK);
+
+    if (ret == -1) {
+      ret = 0;
+      LOG(WARNING) << "Skipping cleanup. Failed to acccess dir=" << mnt 
+        << " errno=" << errno;
+      continue;
+    }
+
     // specified depth-first search FTW_DEPTH
-    // because we can't delete dir unless
+    // because we can't delete mnt unless
     // files in it are deleted
-    ret |= nftw(dir.c_str(),
+    ret |= nftw(mnt.c_str(),
       FileDeleterFunc, 
       numOpenFD, 
       FTW_DEPTH | FTW_MOUNT | FTW_PHYS);
   
-    // WORKAROUND for top-level dir (aka mountpoint) doesn't get removed
-    if (ret == FTW_STOP) 
-    {
-      ret = 0;
-    }
-    if (ret == 0)
-    {
-      LOG(INFO) << "removed all dirs and files under path=" << dir;
-    }
+    if (ret == 0) {
+      LOG(INFO) << "removed all dirs and files under path=" << mnt;
+    } 
   }
   return ret;
 }
