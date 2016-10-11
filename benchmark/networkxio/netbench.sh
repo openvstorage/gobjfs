4f63c10109b53af75307625242c807dc8d33b5d4
Sandeep Joshi <sanjos100@gmail.com>
2016-10-11 12:10:54 +0530

    add option to run with multiple processes to benchmark run script

--- benchmark/networkxio/netbench.sh
+++ benchmark/networkxio/netbench.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+for numprocs in 1 2 3 4
+do
+    echo "numpro" $numprocs
 for numthreads in 1 2 4 8 16 32 
 do
   sed -i "s/max_threads=.*/max_threads=$numthreads/" ./bench_net_client.conf 
-  ./BenchNetClient
+  for (( c = 1; c <= $numprocs; c++))
+  do
+    echo "num_threads="$numthreads "num_process="$c
+    ./BenchNetClient&
+  done
+  wait
+done
 done
