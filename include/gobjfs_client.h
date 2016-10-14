f32f4fafcf6293e629a4d1a05b7d2a9a68ad058e
Sandeep Joshi <sanjos100@gmail.com>
2016-10-14 23:02:34 +0530

    correct javadoc style for @param and @return

--- include/gobjfs_client.h
+++ include/gobjfs_client.h
@@ -52,19 +52,19 @@ struct giocb {
 
 /*
  * Create context attributes object
- * param attr: Context attributes object
- * return: This function always succeeds returning 0
- * return: Context attributes object on success, or NULL on fail
+ * @param attr: Context attributes object
+ * @return: This function always succeeds returning 0
+ * @return: Context attributes object on success, or NULL on fail
  */
 client_ctx_attr_ptr ctx_attr_new();
 
 /*
  * Set transport type
- * param attr: Context attributes object
- * param transport: Transport string ("tcp" or "rdma)
- * param host: Host string FQDN or IP address
- * param port: TCP/RDMA port number
- * return: 0 on success, -1 on fail
+ * @param attr: Context attributes object
+ * @param transport: Transport string ("tcp" or "rdma)
+ * @param host: Host string FQDN or IP address
+ * @param port: TCP/RDMA port number
+ * @return: 0 on success, -1 on fail
  */
 int ctx_attr_set_transport(client_ctx_attr_ptr attr,
                            std::string const &transport,
@@ -72,187 +72,187 @@ int ctx_attr_set_transport(client_ctx_attr_ptr attr,
 
 /*
  * Create gobjfs xio context
- * param attr: Context attributes object
- * return: gobjfs xio context on success, or NULL on fail
+ * @param attr: Context attributes object
+ * @return: gobjfs xio context on success, or NULL on fail
  */
 client_ctx_ptr ctx_new(const client_ctx_attr_ptr attr);
 
 /*
  * Initialize gobjfs xio context to talk to server
- * param ctx: gobjfs xio context
- * return: zero on success, or error code on fail
+ * @param ctx: gobjfs xio context
+ * @return: zero on success, or error code on fail
  */
 int ctx_init(client_ctx_ptr ctx);
 
 /**
  * Get stats for this connection
- * param ctx : gobjfs xio context
- * return : statistics on read requests
+ * @param ctx : gobjfs xio context
+ * @return : statistics on read requests
  */
 std::string ctx_get_stats(client_ctx_ptr ctx);
 
 /*
  * Check connection status
- * param ctx: gobjfs xio context
- * return: True if the client has been disconnected, false otherwise
+ * @param ctx: gobjfs xio context
+ * @return: True if the client has been disconnected, false otherwise
  */
 bool ctx_is_disconnected(client_ctx_ptr ctx);
 
 /*
  * Allocate buffer from the shared memory segment
- * param ctx: gobjfs xio context
- * param size: Buffer size in bytes
- * return: Buffer pointer on success, or NULL on fail
+ * @param ctx: gobjfs xio context
+ * @param size: Buffer size in bytes
+ * @return: Buffer pointer on success, or NULL on fail
  */
 gbuffer *gbuffer_allocate(client_ctx_ptr ctx, size_t size);
 
 /* Retrieve pointer to buffer content
- * param ptr: Pointer to buffer structure
- * return: Buffer pointer on success, or NULL on fail
+ * @param ptr: Pointer to buffer structure
+ * @return: Buffer pointer on success, or NULL on fail
  */
 void *gbuffer_data(gbuffer *ptr);
 
 /* Retrieve size of buffer
- * param ptr: Pointer to buffer structure
- * return: Size of buffer on success, -1 on fail
+ * @param ptr: Pointer to buffer structure
+ * @return: Size of buffer on success, -1 on fail
  */
 size_t gbuffer_size(gbuffer *ptr);
 
 /*
  * Deallocate previously allocated buffer
- * param ctx: gobjfs xio context
- * param shptr: Buffer pointer
- * return: 0 on success, -1 on fail
+ * @param ctx: gobjfs xio context
+ * @param shptr: Buffer pointer
+ * @return: 0 on success, -1 on fail
  */
 int gbuffer_deallocate(client_ctx_ptr ctx, gbuffer *ptr);
 
 /*
  * Read from a volume
- * param ctx: gobjfs xio context
- * param buf: Shared memory buffer
- * param nbytes: Size to read in bytes
- * param offset: Offset to read in volume
- * return: Number of bytes actually read, -1 on fail
+ * @param ctx: gobjfs xio context
+ * @param buf: Shared memory buffer
+ * @param nbytes: Size to read in bytes
+ * @param offset: Offset to read in volume
+ * @return: Number of bytes actually read, -1 on fail
  */
 ssize_t read(client_ctx_ptr ctx, const std::string &filename, void *buf,
              size_t nbytes, off_t offset);
 
 /*
  * Suspend until asynchronous I/O operation or timeout complete
- * param ctx: gobjfs xio context
- * param giocb: Pointer to an AIO Control Block structure
- * param timeout: Pointer to a timespec structure
- * return: 0 on success, -1 on fail
+ * @param ctx: gobjfs xio context
+ * @param giocb: Pointer to an AIO Control Block structure
+ * @param timeout: Pointer to a timespec structure
+ * @return: 0 on success, -1 on fail
  */
 int aio_suspend(client_ctx_ptr ctx, giocb *giocb, const timespec *timeout);
 
 /*
  * Suspend until asynchronous I/O operation or timeout complete
- * param ctx: gobjfs xio context
- * param giocb_vec: Pointer to vector of AIO Control Block structure
- * param timeout: Pointer to a timespec structure
- * return: 0 on success, -1 on fail
+ * @param ctx: gobjfs xio context
+ * @param giocb_vec: Pointer to vector of AIO Control Block structure
+ * @param timeout: Pointer to a timespec structure
+ * @return: 0 on success, -1 on fail
  */
 
 int aio_suspendv(client_ctx_ptr ctx, const std::vector<giocb *> &giocbp_vec,
                  const timespec *timeout);
 /*
  * Retrieve error status of asynchronous I/O operation
- * param ctx: gobjfs xio context
- * param giocb: Pointer to an AIO Control Block structure
- * return: 0 on success, -1 on fail
+ * @param ctx: gobjfs xio context
+ * @param giocb: Pointer to an AIO Control Block structure
+ * @return: 0 on success, -1 on fail
  */
 int aio_error(client_ctx_ptr ctx, giocb *giocbp);
 
 /*
  * Retrieve return status of asynchronous I/O operation
- * param ctx: gobjfs xio context
- * param giocb: Pointer to an AIO Control Block structure
- * return: Number of bytes returned based on the operation, -1 on fail
+ * @param ctx: gobjfs xio context
+ * @param giocb: Pointer to an AIO Control Block structure
+ * @return: Number of bytes returned based on the operation, -1 on fail
  */
 ssize_t aio_return(client_ctx_ptr ctx, giocb *giocbp);
 
 /*
  * Cancel an oustanding asynchronous I/O operation
- * param ctx: gobjfs xio context
- * param giocb: Pointer to an AIO Control Block structure
- * return: 0 on success, -1 on fail
+ * @param ctx: gobjfs xio context
+ * @param giocb: Pointer to an AIO Control Block structure
+ * @return: 0 on success, -1 on fail
  */
 int aio_cancel(client_ctx_ptr ctx, giocb *giocbp);
 
 /*
  * Finish an asynchronous I/O operation
- * param ctx: gobjfs xio context
- * param giocb: Pointer to an AIO Control Block structure
- * return: 0 on success, -1 on fail
+ * @param ctx: gobjfs xio context
+ * @param giocb: Pointer to an AIO Control Block structure
+ * @return: 0 on success, -1 on fail
  */
 int aio_finish(client_ctx_ptr ctx, giocb *giocbp);
 
 /*
  * Asynchronous read from a volume
- * param ctx: gobjfs xio context
- * param filename: filenames on which to read
- * param giocb: Pointer to an AIO Control Block structure
- * return: 0 on success, -1 on fail
+ * @param ctx: gobjfs xio context
+ * @param filename: filenames on which to read
+ * @param giocb: Pointer to an AIO Control Block structure
+ * @return: 0 on success, -1 on fail
  */
 int aio_read(client_ctx_ptr ctx, const std::string &filename, giocb *giocbp);
 
 /*
  * Asynchronous readv from a volume
- * param ctx: gobjfs xio context
- * param filename_vec: Pointer to vector of filenames to read
- * param giocb_vec: Pointer to vector of AIO Control Block structure
+ * @param ctx: gobjfs xio context
+ * @param filename_vec: Pointer to vector of filenames to read
+ * @param giocb_vec: Pointer to vector of AIO Control Block structure
  *   this vector and filename_vec must be same size
- * return: 0 on success, -1 on fail
+ * @return: 0 on success, -1 on fail
  */
 int aio_readv(client_ctx_ptr ctx, const std::vector<std::string> &filename_vec,
               const std::vector<giocb *> &giocbp_vec);
 
 /*
  * Asynchronous read from a volume with completion
- * param ctx: gobjfs xio context
- * param giocb: Pointer to an AIO Control Block structure
- * param completion: Pointer to a completion structure
- * return: 0 on success, -1 on fail
+ * @param ctx: gobjfs xio context
+ * @param giocb: Pointer to an AIO Control Block structure
+ * @param completion: Pointer to a completion structure
+ * @return: 0 on success, -1 on fail
  */
 int aio_readcb(client_ctx_ptr ctx, const std::string &filename, giocb *giocbp,
                completion *completion);
 
 /*
  * Create a new completion
- * param complete_cb: Pointer to an gcallback structure
- * param arg: Pointer to an argument passed to complete_cb
- * return: Completion pointer on success, or NULL on fail
+ * @param complete_cb: Pointer to an gcallback structure
+ * @param arg: Pointer to an argument passed to complete_cb
+ * @return: Completion pointer on success, or NULL on fail
  */
 completion *aio_create_completion(gcallback complete_cb, void *arg);
 
 /*
  * Retrieve return status of a completion
- * param completion: Pointer to a completion structure
- * return: Number of bytes returned based on the operation and the completion,
+ * @param completion: Pointer to a completion structure
+ * @return: Number of bytes returned based on the operation and the completion,
  * -1 on fail
  */
 ssize_t aio_return_completion(completion *completion);
 
 /*
  * Suspend until completion or timeout complete
- * param completion: Pointer to completion structure
- * param timeout: Pointer to a timespec structure
- * return: 0 on success, -1 on fail
+ * @param completion: Pointer to completion structure
+ * @param timeout: Pointer to a timespec structure
+ * @return: 0 on success, -1 on fail
  */
 int aio_wait_completion(client_ctx_ptr& ctx, completion *completion, const timespec *timeout);
 
 /*
  * Signal a suspended completion
- * param completion: Pointer to completion structure
- * return: 0 on success, -1 on fail
+ * @param completion: Pointer to completion structure
+ * @return: 0 on success, -1 on fail
  */
 int aio_signal_completion(completion *completion);
 
 /*
  * Release completion
- * param completion: Pointer to completion structure
- * return: 0 on success, -1 on fail
+ * @param completion: Pointer to completion structure
+ * @return: 0 on success, -1 on fail
  */
 int aio_release_completion(completion *completion);
 }
