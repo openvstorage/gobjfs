b4f75bef542f6aab95db81de87fdb4b190423b00
Sandeep Joshi <sanjos100@gmail.com>
2016-10-30 21:32:07 +0530

    Add test of EPOller with Pipe

--- src/util/test/EPollerTest.cpp
+++ src/util/test/EPollerTest.cpp
@@ -142,3 +142,64 @@ TEST(EPoller, WithTimerNotifier) {
   ret = e1.shutdown();
   EXPECT_EQ(ret, 0);
 }
+
+struct DummyData {
+  int opcode_;
+  int arg_;
+};
+
+static int pipe_read_func(int fd, uintptr_t userData) {
+  uint64_t* numTimesPtr = reinterpret_cast<uint64_t*>(userData);
+  DummyData d;
+  ssize_t ret = read(fd, &d, sizeof(d));
+  EXPECT_EQ(ret, sizeof(d));
+  EXPECT_EQ(d.opcode_, 3);
+  EXPECT_EQ(d.arg_, 4);
+  (*numTimesPtr) ++;
+  return 0;
+}
+
+/**
+ * Check if Pipe interops with EPoller
+ */
+TEST(EPoller, WithPipe) {
+
+  EPoller e1;
+
+  int ret = e1.init();
+  EXPECT_EQ(ret, 0);
+
+  PipeUPtr pipe = gobjfs::make_unique<Pipe>();
+
+  uint64_t numTimesCalled = 0;
+
+  ret = e1.addEvent(reinterpret_cast<uintptr_t>(&numTimesCalled), pipe->getReadFD(), EPOLLIN, pipe_read_func);
+  EXPECT_EQ(ret, 0);
+
+  int write_fd = pipe->getWriteFD();
+
+  auto fut = std::async(std::launch::async, [&write_fd] () {
+ 
+      sleep(5); // wait for epoll to hang in main thread
+
+      DummyData d;
+      d.opcode_ = 3;
+      d.arg_ = 4;
+      ssize_t writeSz = write(write_fd, &d, sizeof(d));
+      EXPECT_EQ(writeSz, sizeof(d));
+    });
+
+  // run loop once
+  ret = e1.run(1);
+  EXPECT_EQ(ret, 0);
+
+  // check if timer handler was called
+  EXPECT_GE(numTimesCalled, 1);
+
+  // drop event which was added
+  ret = e1.dropEvent(reinterpret_cast<uintptr_t>(&numTimesCalled), pipe->getReadFD());
+  EXPECT_EQ(ret, 0);
+
+  ret = e1.shutdown();
+  EXPECT_EQ(ret, 0);
+}
