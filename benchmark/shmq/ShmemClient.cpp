44357b8df3f7077c281277ed54bd7f51151e2d8b
Sandeep Joshi <sanjos100@gmail.com>
2016-11-02 22:31:35 +0530

    expand boost shmem queue benchmark

--- benchmark/shmq/ShmemClient.cpp
+++ benchmark/shmq/ShmemClient.cpp
@@ -1,5 +1,6 @@
 #include <rora/GatewayProtocol.h>
 #include <rora/EdgeQueue.h>
+#include <rora/ASDQueue.h>
 
 #include <util/Timer.h>
 #include <util/Stats.h>
@@ -15,30 +16,72 @@ using namespace gobjfs::os;
 using namespace gobjfs::stats;
 
 EdgeQueueUPtr edgeQueue;
+ASDQueueUPtr asdQueue;
+const std::string asdQueueName = "bench_asd_queue";
+size_t maxAllocSize = 4096;
+size_t maxQueueLen = 10;
 
 int main(int argc, char* argv[])
 {
   int ret = 0;
 
+  int totalCount = 1000000;
+  if (argc > 1) {
+    totalCount = atoi(argv[1]);
+  }
+
+
   int pid = getpid();
   std::cout << "pid=" << pid << std::endl;
 
   // create new for this process
-  edgeQueue = gobjfs::make_unique<EdgeQueue>(pid, 2,
-      GatewayMsg::MaxMsgSize, 4096);
+  edgeQueue = gobjfs::make_unique<EdgeQueue>(pid, maxQueueLen,
+      GatewayMsg::MaxMsgSize, maxAllocSize);
+
+  asdQueue = gobjfs::make_unique<ASDQueue>(asdQueueName);
+
+  {
+    GatewayMsg reqMsg;
+    reqMsg.edgePid_ = pid;
+    reqMsg.opcode_ = ADD_EDGE_REQ;
+    ret = asdQueue->write(reqMsg);
+    assert(ret == 0);
+
+    GatewayMsg respMsg;
+    ret = edgeQueue->read(respMsg);
+    assert(ret == 0);
+  }
 
   Timer throughputTimer(true);
 
   size_t doneCount = 0;
 
-  for (doneCount = 0; doneCount < 1000; doneCount ++) {
-    GatewayMsg msg;
-    const auto ret = edgeQueue->write(msg);
+  for (doneCount = 0; doneCount < totalCount; doneCount ++) {
+
+    GatewayMsg reqMsg;
+    reqMsg.edgePid_ = pid;
+    ret = asdQueue->write(reqMsg);
+    assert(ret == 0);
+
+    GatewayMsg respMsg;
+    ret = edgeQueue->read(respMsg);
     assert(ret == 0);
   }
 
   int64_t timeMilli = throughputTimer.elapsedMilliseconds();
   float iops = ((float)doneCount * 1000) / timeMilli;
+
+  {
+    GatewayMsg reqMsg;
+    reqMsg.edgePid_ = pid;
+    reqMsg.opcode_ = DROP_EDGE_REQ;
+    ret = asdQueue->write(reqMsg);
+    assert(ret == 0);
+
+    GatewayMsg respMsg;
+    ret = edgeQueue->read(respMsg);
+    assert(ret == 0);
+  }
 
   edgeQueue.reset();
 
--- benchmark/shmq/ShmemServer.cpp
+++ benchmark/shmq/ShmemServer.cpp
@@ -1,5 +1,6 @@
 #include <rora/GatewayProtocol.h>
 #include <rora/EdgeQueue.h>
+#include <rora/ASDQueue.h>
 
 #include <util/Timer.h>
 #include <util/Stats.h>
@@ -14,29 +15,61 @@ using namespace gobjfs::rora;
 using namespace gobjfs::os;
 using namespace gobjfs::stats;
 
-EdgeQueueUPtr edgeQueue;
+std::map<int, EdgeQueueUPtr> edgeQueueMap;
+ASDQueueUPtr asdQueue;
+size_t maxQueueLen = 256;
+const std::string asdQueueName = "bench_asd_queue";
 
 int main(int argc, char* argv[])
 {
   int ret = 0;
 
-  int pid = atoi(argv[1]);
+  asdQueue = gobjfs::make_unique<ASDQueue>(asdQueueName, maxQueueLen, GatewayMsg::MaxMsgSize);
 
-  edgeQueue = gobjfs::make_unique<EdgeQueue>(pid);
+  while (1) {
 
-  Timer throughputTimer(true);
-
-  size_t doneCount = 0;
-
-  for (doneCount = 0; doneCount < 1000; doneCount ++) {
-    GatewayMsg responseMsg;
-    const auto ret = edgeQueue->read(responseMsg);
+    GatewayMsg reqMsg;
+    ret = asdQueue->read(reqMsg);
     assert(ret == 0);
+
+    switch (reqMsg.opcode_) { 
+
+      case ADD_EDGE_REQ : 
+        {
+          auto edgeQueue = gobjfs::make_unique<EdgeQueue>(reqMsg.edgePid_);
+      
+          GatewayMsg respMsg;
+          ret = edgeQueue->write(respMsg);
+          assert(ret == 0);
+
+          std::cout << "added edgeQueue for pid=" << reqMsg.edgePid_ << std::endl;
+          edgeQueueMap.insert(std::make_pair(reqMsg.edgePid_, std::move(edgeQueue)));
+          break;
+        }
+      case DROP_EDGE_REQ : 
+        {
+          auto iter = edgeQueueMap.find(reqMsg.edgePid_);
+          assert(iter != edgeQueueMap.end());
+          auto& edgeQueue = iter->second;
+
+          GatewayMsg respMsg;
+          ret = edgeQueue->write(respMsg);
+          assert(ret == 0);
+
+          edgeQueueMap.erase(iter);
+          std::cout << "dropped edgeQueue for pid=" << reqMsg.edgePid_ << std::endl;
+          break;
+        }
+      default : 
+        {
+          GatewayMsg respMsg;
+          auto iter = edgeQueueMap.find(reqMsg.edgePid_);
+          assert(iter != edgeQueueMap.end());
+          auto& edgeQueue = iter->second;
+          ret = edgeQueue->write(respMsg);
+          assert(ret == 0);
+          break;
+        }
+    }
   }
-
-  int64_t timeMilli = throughputTimer.elapsedMilliseconds();
-  float iops = ((float)doneCount * 1000) / timeMilli;
-
-  edgeQueue.reset();
-  std::cout << "iops=" << iops << std::endl;
 }
