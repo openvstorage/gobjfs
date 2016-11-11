234303d03fdc63739827b697da694455bf330ac9
Sandeep Joshi <sanjos100@gmail.com>
2016-11-11 09:06:20 +0000

    added run identifier as first arg to bench.sh

--- benchmark/rora/bench.sh
+++ benchmark/rora/bench.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+  echo "supply run identifier as first arg"
+  exit 1
+fi
+
 for numprocs in 8 16 32 1 2 4
 do
   for (( c = 1; c <= $numprocs; c++))
   do
-    ./EdgeProcess async1 $numprocs &
+    ./EdgeProcess $1 $numprocs &
   done
   wait
 done
